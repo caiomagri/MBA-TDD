b9d072282ef7541f239233d79b41f340044ad241
Caio Magri <kaioeduardofm@gmail.com>
2022-05-14 17:53:43 -0300

    Correcting view to deal with normal request and request via POST

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,6 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 
-from lists.models import Item
-
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -14,10 +12,22 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEquals(Item.objects.count(), 0)
+
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEquals(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEquals(new_item.text, "A new list item")
+
         self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
+
+
+from lists.models import Item
 
 
 class ItemModelTest(TestCase):
